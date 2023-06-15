32325f8fdc7d673d03aee5129e113b6c7f6f3e1f
Zijian Zhang <doomspec@outlook.com>
2023-06-15 18:59:32 +0800

    Fix

--- playground/hello_world.py
+++ playground/hello_world.py
@@ -1,19 +1,8 @@
-from evomark import show, evolve, ask, answer, out
+from evomark import show, evolve, answer
 
-a = ask("Who is M. Foucault?")
+a = answer("Who is M. Foucault?")
 
+# Run and Evolve!
 show(a)
-"""""show
-user: Who is M. Foucault?
-"""""
-
-a = answer(a)
-
-show(a)
-"""""show
-M. Foucault refers to Michel Foucault, a French philosopher, social theorist, and historian who is known for his influential works on power, sexuality, and knowledge. He was born on October 15, 1926, in Poitiers, France, and died on June 25, 1984. His major works include Madness and Civilization, The Birth of the Clinic, Discipline and Punish, and The History of Sexuality. Foucault's ideas have had a significant impact on various disciplines, including philosophy, sociology, psychology, cultural studies, and political science.
-"""""
-
-out(a)
 
 evolve()
--- src/evomark/__init__.py
+++ src/evomark/__init__.py
@@ -1,7 +1,7 @@
 from evomark.core import EvoCore
 EvolverInstance = EvoCore()
 
-from evomark.core._E import show, let, evolve, retake
+from evomark.core.evolver import show, let, evolve, retake
 from evomark.model.openai import answer
 from evomark.data_type.key_value import question, topic
 from evomark.io.out import out, out_cl, set_out_path
